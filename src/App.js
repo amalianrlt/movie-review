1fcde58e5e848d899e91a96bda5182e97d99f0e5
Amalianrlt <amalianurlita97@gmail.com>
2020-06-09 11:46:12 +0700

    [Amal] - creating UI

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import './App.css';
 import LoginForm from './components/LoginForm';
 import HomePage from './modules/HomePage'
 import DetailOverview from './modules/DetailOverview'
+import OverviewTab from './components/OverviewTab';
 
 function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <LoginForm/>
-        <DetailOverview/>
+        {/* <LoginForm/> */}
+        <HomePage/>
       </div>
     </Provider>
   );
--- src/modules/HomePage.js
+++ src/modules/HomePage.js
@@ -3,11 +3,13 @@ import HeaderPage from '../components/HeaderPage'
 import FooterPage from '../components/FooterPage'
 import HomeTab from '../components/HomeTab'
 import PaginationPage from '../components/Pagination'
+import CarouselHome from '../components/CarouselHome'
 
 export default function HomePage() {
     return (
         <div>
             <HeaderPage/>
+            <CarouselHome/>
             <HomeTab/>
             <PaginationPage/>
             <FooterPage/>
